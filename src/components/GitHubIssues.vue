0b34239d53ef311ccf8257f4e2d228d1010c96f4
Hugo Didatico <hugo@didaticotecnologia.com.br>
2019-09-02 17:48:06 -0300

    Aula 32 - Vue.js + Github issues 3

---
+++ src/components/GitHubIssues.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container">
+        <h1>Hello</h1>
+        <p class="lead">
+            Página que lista issues de um repositório do Github, usando Vue.js.
+        </p>
+
+        <div class="row">
+            <div class="col">
+                <div class="form-group">
+                    <input v-model="username"
+                        type="text" class="form-control" placeholder="github username">
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="form-group">
+                    <input v-model="repository"
+                        type="text" class="form-control" placeholder="github repositório">
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="form-group">
+                    <button @click.stop.prevent="getIssues()" class="btn btn-success">GO</button>
+                    <button @click.stop.prevent="reset()" class="btn btn-danger">LIMPAR</button>
+                </div>
+            </div>
+        </div>
+
+        <br><hr><br>
+
+        <table class="table table-sm table-bordered">
+            <thead>
+            <tr>
+                <th width="100">Número</th>
+                <th>Título</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            <tr v-if="loader.getIssues">
+              <td class="text-center" colspan="2"><img src="/static/download.svg" alt=""></td>
+            </tr>
+            <tr v-if="!!issues.length && !loader.getIssues"
+                v-for="issue in issues"
+                :key="issue.number">
+                <td> {{ issue.number }} </td>
+                <td> {{ issue.title }} </td>
+            </tr>
+
+            <tr v-if="!!!issues.length && !loader.getIssues">
+                <td class="text-center" colspan="2">Nenhuma issues encontrada!</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default{
+  name: 'GitHubIssues',
+  data() {
+    return {
+      username: '',
+      repository: '',
+      issues: [],
+      loader: {
+        getIssues: false,
+      },
+    };
+  },
+  methods: {
+    reset() {
+      this.username = '';
+      this.repository = '';
+    },
+    getIssues() {
+      if (this.username && this.repository) {
+        this.loader.getIssues = true;
+        const url = `https://api.github.com/repos/${this.username}/${this.repository}/issues`;
+        axios.get(url).then((response) => {
+          this.issues = response.data;
+        }).finally(() => {
+          this.loader.getIssues = false;
+        });
+      }
+    },
+  },
+};
+</script>
